131792d0e2578d792ab93fd697c080a0ddea3f90
Ikraam <ibndaanis@gmail.com>
2024-07-10 18:31:51 +0100

    added typography

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-// load-header.js
-document.addEventListener("DOMContentLoaded", function () {
-  const components = ["header", "buttons"].forEach((component) => {
-    fetch(`/components/${component}.html`)
-      .then((response) => response.text())
-      .then((data) => {
-        const headerPlaceholder = document.getElementById(
-          `${component}-placeholder`
-        );
+const components = ["header", "typography", "buttons"].forEach((component) => {
+  fetch(`/components/${component}.html`)
+    .then((response) => response.text())
+    .then((data) => {
+      const headerPlaceholder = document.getElementById(
+        `${component}-placeholder`
+      );
 
-        if (headerPlaceholder) {
-          headerPlaceholder.innerHTML = data;
-        }
-      })
-      .catch((error) => console.error(`Error loading ${component}:`, error));
-  });
+      if (headerPlaceholder) {
+        headerPlaceholder.innerHTML = data;
+      }
+    })
+    .catch((error) => console.error(`Error loading ${component}:`, error));
 });
