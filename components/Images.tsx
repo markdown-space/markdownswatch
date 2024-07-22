049a08e91cd4d61136d5890c5f41eeec0df6e69f
Ikraam <ibndaanis@gmail.com>
2024-07-22 20:45:43 +0100

    fixed image link

--- components/Images.tsx
+++ components/Images.tsx
@@ -4,7 +4,7 @@ export const Images = () => {
       <h1 className="title"> Images </h1>
       <hr />
       <figure className="image is-128x128">
-        <img src="https://s3.amazonaws.com/uifaces/faces/twitter/zeldman/128.jpg" />
+        <img src="https://media.licdn.com/dms/image/C560BAQHPEhawj1hjIg/company-logo_200_200/0/1630670195428?e=2147483647&v=beta&t=okitgnrzLVM2V9Uhrt8XslRihTIv6L2B7eH5AM7ATIE" />
       </figure>
     </section>
   );
